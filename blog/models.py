2d2d6a235aafeeeffe24c6eb5ead150449c893d8
Silvia Saverino <silviasave89@gmail.com>
2023-01-15 17:24:39 +0000

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Main Table model post
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Table model for comments
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+class User(models.Model):
+    """
+    Table model to represent the site user
+    """
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Movie(models.Model):
+    """
+    Table model represent the site movie
+    """
+    title = models.CharField(max_length=70)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=70)
+    director = models.CharField(max_length=70)
+
+
+class Reviews(models.Model):
+    """
+    Table model represent the site reviews
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review_content = models.TextField(max_length=255)
