3a1da62bc8c96fb3b10b10577e6f8b753dc00c31
Silvia Saverino <silviasave89@gmail.com>
2023-01-19 21:43:15 +0000

    fix typos in urls path

--- blog/views.py
+++ blog/views.py
@@ -71,7 +71,7 @@ class PostLike(View):
         post = get_object_or_404(Post, slug=slug)
 
         if post.likes.filter(id=request.user.id).exists():
-            post.likes.reomve(request.user)
+            post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
             
