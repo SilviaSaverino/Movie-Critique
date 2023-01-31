5b1dbdf6d43b184d00fb61912f9847d117765533
Silvia Saverino <silviasave89@gmail.com>
2023-01-31 16:14:13 +0000

    add class form for director request

--- blog/forms.py
+++ blog/forms.py
@@ -8,13 +8,18 @@ class CommentForm(forms.ModelForm):
         fields = ('body',)
         
         
-class MovieGenreForm(forms.ModelForm):
-    class Meta:
-        model = MovieGenre
-        fields = ['genre_name']
+# class MovieGenreForm(forms.ModelForm):
+#     class Meta:
+#         model = MovieGenre
+#         fields = ['genre_name']
         
-    def clean_genre_name(self):
-        genre_name = self.cleaned_data.get('genre_name')
-        if MovieGenre.objects.filter(genre_name=genre_name).exists():
-            raise forms.ValidationError("A genre with this name already exists.")
-        return genre_name
+#     def clean_genre_name(self):
+#         genre_name = self.cleaned_data.get('genre_name')
+#         if MovieGenre.objects.filter(genre_name=genre_name).exists():
+#             raise forms.ValidationError("A genre with this name already exists.")
+#         return genre_name
+
+class DirectorRequestForm(forms.ModelForm):
+    class Meta:
+        model = UserRequest
+        fields = ['director_name', 'genre', 'bio']
