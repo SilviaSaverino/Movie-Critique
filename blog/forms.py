25ac07e8392713eca000e9b89faa743d2870396b
Silvia Saverino <silviasave89@gmail.com>
2023-02-03 20:03:14 +0000

    add list item to base html to diplay user requests

--- blog/forms.py
+++ blog/forms.py
@@ -7,17 +7,6 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('body',)
         
-        
-# class MovieGenreForm(forms.ModelForm):
-#     class Meta:
-#         model = MovieGenre
-#         fields = ['genre_name']
-        
-#     def clean_genre_name(self):
-#         genre_name = self.cleaned_data.get('genre_name')
-#         if MovieGenre.objects.filter(genre_name=genre_name).exists():
-#             raise forms.ValidationError("A genre with this name already exists.")
-#         return genre_name
 
 class UserRequestForm(forms.ModelForm):
     class Meta:
