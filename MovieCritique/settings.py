64aa43130d40f3416bff4b543e68bc3ea0457ff9
Silvia Saverino <silviasave89@gmail.com>
2023-02-20 22:18:55 +0000

    add initial content and initial style to 404 page

--- MovieCritique/settings.py
+++ MovieCritique/settings.py
@@ -29,11 +29,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['movie-critique-project.herokuapp.com', 'localhost', '*']
+ALLOWED_HOSTS = ['movie-critique-project.herokuapp.com', 'localhost']
 
 
 # Application definition
--- blog/urls.py
+++ blog/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
     path('<slug:slug>/', views.PostDetail.as_view(), name="post_detail"),
     path('like/<slug:slug>', views.PostLike.as_view(), name="post_like"),
-   
+
 ]
 
 # 404 errors
